d3baf94deea517496217806f289c86976733fba4
Joy O'Halloran <johalloran@protonmail.com>
2025-01-30 12:11:00 -0800

    done with gallery

--- react-vite/src/components/Gallery/Gallery.jsx
+++ react-vite/src/components/Gallery/Gallery.jsx
@@ -1,12 +1,15 @@
 import { galleryURLs } from '../../data.json';
+import './Gallery.css';
 
 export default function Gallery() {
   return (
     <>
       <h1>Gallery</h1>
-      {galleryURLs.map((url, i) => (
-        <img src={url} key={i} />
-      ))}
+      <div className='gallery-wrapper'>
+        {galleryURLs.map((url, i) => (
+          <img src={url} key={i} className='gallery-image' />
+        ))}
+      </div>
     </>
   );
 }
