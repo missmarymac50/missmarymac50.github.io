92594fd8c7a21fd62e959956170cb690e3c34d33
Joy O'Halloran <johalloran@protonmail.com>
2025-01-30 12:28:21 -0800

    added photo credit

--- react-vite/src/components/Gallery/Gallery.jsx
+++ react-vite/src/components/Gallery/Gallery.jsx
@@ -10,6 +10,7 @@ export default function Gallery() {
           <img src={url} key={i} className='gallery-image' />
         ))}
       </div>
+      <footer className='photos-credit'>Photos by Joe Mazza</footer>
     </>
   );
 }
